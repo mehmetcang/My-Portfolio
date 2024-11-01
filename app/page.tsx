82c7f3e3f9ef594adadef7f4cd02c4a6a92ae22d
Mehmetcan Gültekin <gultekin55mhmt@gmail.com>
2024-11-01 20:12:04 +0300

    Fix ESLint configuration and package.json dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 'use client';
 
 import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
-import MotionSection from '../components/MotionSection';
 import Motion from '../components/Motion';
 import dynamic from 'next/dynamic';
 
